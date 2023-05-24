26cd440d8777c7176bf930081295a1470676cf4f
AniketShinde9598 <aniketshinde9598@gmail.com>
2023-05-24 21:55:04 +0530

    redux done.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import React from "react";
-import ListDemo from "./imgcomponents/ListDemo";
+import { useDispatch, useSelector } from "react-redux";
+import { deposit, withdraw } from "./redux/accountSlice";
 
 function App() {
   return (
     <div>
-      <ListDemo />
+      <DepositAccount />
+      <hr />
+      <WithdrawlAccount />
+    </div>
+  );
+}
+
+function DepositAccount() {
+  // State is global here
+  let { account } = useSelector((state) => state);
+  let dispatch = useDispatch();
+
+  let depositHandler = () => {
+    dispatch(deposit());
+  };
+
+  return (
+    <div>
+      <h1>Account: CDAC</h1>
+      <h1>Balance: {account.balance}</h1>
+      <input type="button" value="Deposit" onClick={depositHandler} />
+    </div>
+  );
+}
+
+function WithdrawlAccount() {
+  // State is global here
+  let { account } = useSelector((state) => state);
+  let dispatch = useDispatch();
+
+  let withdrawHandler = () => {
+    dispatch(withdraw());
+  };
+
+  return (
+    <div>
+      <h1>Account: CDAC</h1>
+      <h1>Balance: {account.balance}</h1>
+      <input type="button" value="Withdraw" onClick={withdrawHandler} />
     </div>
   );
 }
